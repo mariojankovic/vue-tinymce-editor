c80237513e01c6cf365c83b586114f0328c6a4d6
Mario Jankovic <majankovi@gmail.com>
2017-10-23 15:41:53 +0200

    Removed CSS interfering with the document styles

--- src/components/TinymceVue.vue
+++ src/components/TinymceVue.vue
@@ -58,7 +58,7 @@
     import 'tinymce/plugins/visualchars';
     
     import 'tinymce/skins/lightgray/skin.min.css'
-    import 'tinymce/skins/lightgray/content.min.css'
+    // import 'tinymce/skins/lightgray/content.min.css'
    
     export default {
         name: 'tinymce',
